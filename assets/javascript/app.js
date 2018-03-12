41dcb4ef6fce0911c7abafbb3d70d08bdcf67e1f
Brock Belmonte <brockbel@gmail.com>
2018-03-12 17:46:02 -0500

    This is the beginning of the app and linking the js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//Declaring variables
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var playerOne = "";
+var playerTwo = "";
+var choiceOne = "";
+var choiceTwo = "";
+
+
+
+//This function will detect the click and report the choice
+$(".choices").on("click", function(e){
+    console.log("Yay you made a click event, who da man? You da man!")
+});
